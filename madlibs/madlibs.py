ca05a8fe10b843617f16d9d83de137a1635f5048
Sachi <sdp0186@gmail.com>
2022-04-20 07:04:23 -0400

    commit on main

---
+++ madlibs/madlibs.py
@@ -0,0 +1,18 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+noun = input("Noun_Person: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+noun5 = input("Noun: ")
+famous_person = input("Famous Person: ")
+
+
+madlib = f"I got into computer programming because it is so {adj}. It makes me want to {verb1}. When I start typing, \
+I forget about time. My {noun} has to remind me to {verb2} otherwise I will look like a {noun2}. \
+{famous_person} once said 'Be sure to get some sleep, or else you will look like a {noun3}.'\
+I laughed at that one. How could I look like {noun3}, when I am a girl with short hair. My {noun} gave \
+me a {noun4} when I am spending to much time coding. I love {noun5}!"
+
+print(madlib)
